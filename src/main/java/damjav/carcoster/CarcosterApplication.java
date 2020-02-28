4a931a6adf385cd472f3382298814f682503357d
DamJav <damian.garbacz.9@gmail.com>
2020-02-28 12:41:25 +0100

    Budowa struktury projektu..

---
+++ src/main/java/damjav/carcoster/CarcosterApplication.java
@@ -0,0 +1,13 @@
+package damjav.carcoster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarcosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarcosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/damjav/carcoster/CarcosterApplicationTests.java
@@ -0,0 +1,13 @@
+package damjav.carcoster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
