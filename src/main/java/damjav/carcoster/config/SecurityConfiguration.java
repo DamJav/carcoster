4631e0e092698b211f11e04b4defaa19592a4294
DamJav <damian.garbacz.9@gmail.com>
2020-03-01 11:44:16 +0100

    Spring security klasa cd..

--- src/main/java/damjav/carcoster/config/SecurityConfiguration.java
+++ src/main/java/damjav/carcoster/config/SecurityConfiguration.java
@@ -26,15 +26,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(passwordEncoder())
-                .usersByUsernameQuery("SELECT username, password, active FROM users WHERE username = ?")
-                .authoritiesByUsernameQuery("SELECT u.username, r.name FROM users u JOIN users_roles ur ON u.id" +
-                        "= ur.user_id JOIN roles r ON ur.roles_id = r.id WHERE u.username = ?");
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .passwordEncoder(passwordEncoder())
+//                .usersByUsernameQuery("SELECT username, password, active FROM users WHERE username = ?")
+//                .authoritiesByUsernameQuery("SELECT u.username, r.name FROM users u JOIN users_roles ur ON u.id" +
+//                        "= ur.user_id JOIN roles r ON ur.roles_id = r.id WHERE u.username = ?");
+//    }
 
 
     @Override
@@ -48,16 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                // .antMatchers("/user", "/user/*", "/user/**")
-                    .antMatchers("/").permitAll()
-                .antMatchers("/register").permitAll()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/media/**").permitAll()
-                .antMatchers("/more/**").permitAll()
-                .antMatchers("/logout").authenticated()
-                .antMatchers("/advertisements/**").permitAll()
-                .antMatchers("/user", "/user/**").hasRole("USER")
-                .antMatchers("/admin", "/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
+                    .antMatchers("/**").permitAll()
                 .and()
             .formLogin()
                 .loginPage("/login")
