f1c4c6ab0892f1290d63bbf408f41c70ec67d069
DamJav <damian.garbacz.9@gmail.com>
2020-03-04 11:21:30 +0100

    UserRepo

--- src/main/java/damjav/carcoster/repositories/UserRepository.java
+++ src/main/java/damjav/carcoster/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package damjav.carcoster.repositories;
 
-public interface UserRepository {
+
+import damjav.carcoster.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
 }
