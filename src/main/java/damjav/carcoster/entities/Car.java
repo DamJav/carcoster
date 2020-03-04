3089e1dfff8057860afd6b601cd74c6d15ee8724
DamJav <damian.garbacz.9@gmail.com>
2020-03-04 11:11:50 +0100

    Car entity - done

--- src/main/java/damjav/carcoster/entities/Car.java
+++ src/main/java/damjav/carcoster/entities/Car.java
@@ -1,4 +1,22 @@
 package damjav.carcoster.entities;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cars")
 public class Car {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String brand;
+    private String model;
+    private Integer year;
+    private Integer milage;
+
+
 }
